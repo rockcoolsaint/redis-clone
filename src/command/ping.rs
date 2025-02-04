13d75d73cc5f11470cd4d5fe2ab1081bee4c6100
Innocent Onyemaenu <rockcoolsaint@gmail.com>
2025-02-04 14:55:00 +0100

    Ping command module

---
+++ src/command/ping.rs
@@ -0,0 +1,49 @@
+use crate::resp::types::RespType;
+
+use super::CommandError;
+
+/// Represents the PING command in Redis-clone
+#[derive(Debug, Clone)]
+pub struct Ping {
+  /// Custom message
+  msg: Option<String>
+}
+
+impl Ping {
+  /// Creates a new `Ping` instance from the given arguments.
+  ///
+  /// # Arguments
+  ///
+  /// * `args` - A vector of `RespType` representing the arguments to the PING command.
+  ///
+  /// # Returns
+  ///
+  /// * `Ok(Ping)` if parsing succeeds.
+  pub fn with_Args(args: Vec<RespType>) -> Result<Ping, CommandError> {
+    if args.len() == 0 {
+      return Ok(Ping { msg: None })
+    }
+
+    let msg = match &args[0] {
+      RespType::BulkString(s) => s.clone(),
+      _ => return Err(CommandError::Other(String::from("Invalid message"))),
+    };
+
+    Ok(Ping { msg: Some(msg) })
+  }
+
+  /// Executes the PING command.
+  ///
+  /// # Returns
+  ///
+  /// A `RespType` representing the response:
+  /// - If no message was provided, it returns "PONG" as a `SimpleString`.
+  /// - If a message was provided, it returns that message as a `BulkString`.
+  pub fn apply(&self) -> RespType {
+    if let Some(msg) = &self.msg {
+      RespType::BulkString(msg.to_string())
+    } else {
+      RespType::SimpleString(String::from("PONG"))
+    }
+  }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,29 @@
+mod command;
 mod server;
 mod resp;
 mod handler;
 
 use anyhow::Result;
+use clap::Parser;
 use log::info;
 use server::Server;
 use tokio::net::TcpListener;
+
+const DEFAULT_PORT: u16 = 6377;
+
+#[derive(Debug, Parser)]
+#[command(
+    name = "redis-clone-server",
+    version,
+    author,
+    about = "A RESP based in-memory cache"
+)]
+struct Cli {
+    /// Port to be bound to Nimblecache server
+    #[arg(long)]
+    port: Option<u16>,
+}
+
 
 #[tokio::main]
 async fn main() -> Result<()> {
@@ -13,15 +31,19 @@ async fn main() -> Result<()> {
     // This sets up logging based on the RUST_LOG environment variable
     env_logger::init();
 
+    // Get port from --port CLI parameter. Defaults to 6377
+    let cli = Cli::parse();
+    let port = cli.port.unwrap_or(DEFAULT_PORT);
+
     // Define the address and port for the TCP server to listen on
     // Here we're using localhost (127.0.0.1) and port 6379 (commonly used for Redis)
-    let addr = format!("127.0.0.1:{}", 6377);
+    let addr = format!("127.0.0.1:{}", port);
 
     // Attempt to bind the TCP listener to the specified address and port
     let listener = match TcpListener::bind(&addr).await {
         // if successful, return the TcpListener
         Ok(tcp_listener) => {
-            info!("TCP Listener started on port 6377");
+            info!("TCP Listener started on port port");
             tcp_listener
         },
         // If there is an error, panic and print the error message
